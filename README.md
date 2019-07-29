96de6efd8c38f4d2fb20780f6506e2f788738dce
Caleb Myers <cmyers5108@gmail.com>
2019-07-28 20:09:17 -0400

    update README

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # liri-node-app
+
+Caleb Myers
+
+### Overview
+
+This is a command line LIRI program designed to access data from Spotify, BandsInTown, and IMDb. This allows users to retrieve data from these three sources in one program with easy to use commands.
+
+There are several commands available in the program that users may choose from.
+
+ * `concert-this`
+ * `spotify-this-song`
+ * `movie-this`
+ * `do-what-it-says`
+
+To begin, navigate to your directory containing the program files. You will need three pieces of information to run a command:
+
+ * The file name -- `liri.js`
+
+ * Which command you want to run
+
+ * A term to search for
+
+These three items will allow you to make a command with LIRI. (NOTE: `do-what-it-says` does not require a search term after the command)
+
+### Examples
+
+Making a search to BandsInTown for Red Hot Chili Peppers
+
+ * `node liri.js concert-this Red Hot Chili Peppers`
+
+![concert-this working](./images/concert-working)
+
+Making a search to Spotify for the song "Wish You Were Here"
+
+ * `node liri.js spotify-this-song Wish You Were Here`
+
+Making a search to IMDb for The Goonies
+
+ * `node liri.js movie-this The Goonies`
+
+Running the command listed in `random.txt`
+
+ * `node do-what-it-says`
+
+### Details
+
+Tech
+
+Process
--- liri.js
+++ liri.js
@@ -1,32 +1,30 @@
 require("dotenv").config()
+var fs = require("fs")
 var axios = require("axios")
 var moment = require("moment")
 var keys = require("./keys.js")
 var Spotify = require("node-spotify-api")
 var spotify = new Spotify(keys.spotify)
-var fs = require("fs")
 
 function bandsSearch(keyword) {
     axios
         .get("https://rest.bandsintown.com/artists/" + keyword + "/events?app_id=codingbootcamp")
-        .then(function (response) {
-            // console.log(response)
-            if (Array.isArray(response.data) && response.data.length > 0) {
-                for (let i = 0; i < response.data.length; i++) {
+        .then(function (res) {
+            console.log("Searching for " + keyword + "...")
+            if (Array.isArray(res.data) && res.data.length > 0) {
+                for (let i = 0; i < res.data.length; i++) {
                     console.log("===================================")
                     console.log("Event #", (i + 1), "for", keyword)
-                    console.log("Venue:", response.data[i].venue.name)
-                    console.log("Location:", response.data[i].venue.city, response.data[i].venue.region)
-                    console.log("Date:", moment(response.data[i].datetime, "YYYY-MM-DD").format("MM/DD/YYYY"))
+                    console.log("Venue:", res.data[i].venue.name)
+                    console.log("Location:", res.data[i].venue.city, (res.data[i].venue.region || res.data[i].venue.country))
+                    console.log("Date:", moment(res.data[i].datetime, "YYYY-MM-DD").format("MM/DD/YYYY"))
                 }
-            } else if (response.data.length === 0) {
-                console.log("Please enter a search term after the command.")
             } else {
                 console.log("No matches found!")
             }
         })
         .catch(function (err) {
-            console.log(err.response.data.errorMessage)
+            console.log(err.response.data.message || err.response.data.errorMessage)
         })
 }
 
@@ -36,14 +34,27 @@ function spotifySearch(keyword) {
             type: "track",
             query: keyword || "The Sign Ace of Base",
         })
-        .then(function (response) {
-            console.log("Artist: " + response.tracks.items[0].artists[0].name)
-            console.log("Song: " + response.tracks.items[0].name)
-            console.log("Link: " + response.tracks.items[0].preview_url)
-            console.log("Album: " + response.tracks.items[0].album.name)
+        .then(function (res) {
+            console.log("Searching for " + (keyword || "The Sign by Ace of Base") + "...")
+            if (res.tracks.items.length > 0) {
+                for (let i = 0; i < res.tracks.items.length; i++) {
+                    console.log("===================================")
+                    console.log("Track #", (i + 1), "for:", (keyword || "The Sign by Ace of Base"))
+    
+                    for (let j = 0; j < res.tracks.items[i].artists.length; j++) {
+                        console.log("Artist:", res.tracks.items[i].artists[j].name)
+                    }
+    
+                    console.log("Song:", res.tracks.items[i].name)
+                    console.log("Link:", (res.tracks.items[i].preview_url || "Unavailable"))
+                    console.log("Album:", res.tracks.items[i].album.name)
+                }
+            } else {
+                console.log("No matches found!")
+            }
         })
         .catch(function (err) {
-            console.log("Error occurred: " + err)
+            console.log("Error occurred:", err)
         })
 }
 
@@ -51,26 +62,30 @@ function omdbSearch(keyword) {
     axios
         .get("http://www.omdbapi.com/?t=" + (keyword || "Mr Nobody") + "&y=&plot=short&apikey=trilogy")
         .then(function (response) {
+            console.log("Searching for " + (keyword || "Mr Nobody") + "...")
             if (response.data.Response === "True") {
-                console.log("Movie Title: " + response.data.Title)
-                console.log("Year Released: " + response.data.Year)
-                console.log("IMDB Rating: " + response.data.imdbRating)
-                console.log(response.data.Ratings[1].Source + " Rating: " + response.data.Ratings[1].Value)
-                console.log("Produced in: " + response.data.Country)
-                console.log("Available Language(s):  " + response.data.Language)
-                console.log("Plot Summary: " + response.data.Plot)
-                console.log("Cast Members: " + response.data.Actors)
+                console.log("Movie Title:", response.data.Title)
+                console.log("Year Released:", response.data.Year)
+                console.log("IMDB Rating:", response.data.imdbRating)
+
+                for (let i = 0; i < response.data.Ratings.length; i++) {
+                    if (response.data.Ratings[i].Source === "Rotten Tomatoes") {
+                        console.log(response.data.Ratings[i].Source, "Rating:", response.data.Ratings[1].Value)
+                    }
+                }
+
+                console.log("Produced in:", response.data.Country)
+                console.log("Available Language(s):", response.data.Language)
+                console.log("Plot Summary:", response.data.Plot)
+                console.log("Cast Members:", response.data.Actors)
             } else {
                 console.log("No matches found!")
             }
         })
         .catch(function (err) {
             if (err.response) {
-                console.log("---------------Data---------------")
                 console.log(err.response.data)
-                console.log("---------------Status---------------")
                 console.log(err.response.status)
-                console.log("---------------Status---------------")
                 console.log(err.response.headers)
             } else if (err.request) {
                 console.log(err.request)
@@ -82,7 +97,7 @@ function omdbSearch(keyword) {
 }
 
 function randomSearch() {
-    fs.readFile("random.txt", "utf8", function(error, data) {
+    fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
             return console.log(error)
         }
@@ -97,32 +112,22 @@ function processCommand(command, input) {
         case "concert-this":
             bandsSearch(input)
             break;
-    
+
         case "spotify-this-song":
             spotifySearch(input)
             break;
-    
+
         case "movie-this":
             omdbSearch(input)
             break;
-    
+
         case "do-what-it-says":
             randomSearch()
             break;
-    
+
         default:
             console.log("Not a recognized command")
     }
 }
 
-processCommand(process.argv[2], process.argv[3])
-
-
-/*
-
-BUGS:
--- Difference between error responses
-    -- asdff == no matches found
-    -- asdfad == error given
-
-*/
+processCommand(process.argv[2], process.argv.slice(3).join(" "))
